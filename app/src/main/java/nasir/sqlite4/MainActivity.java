3e0ea55bdb80708b131fe947ccf067a72bd1d03d
nazmul-nasir <nazmul.nasir@gmail.com>
2016-01-06 10:54:12 +0600

    Initial commit

---
+++ app/src/main/java/nasir/sqlite4/MainActivity.java
@@ -0,0 +1,49 @@
+package nasir.sqlite4;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity  extends Activity {
+
+    /** Called when the activity is first created. */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView contactView = (TextView) findViewById(R.id.contactview);
+
+        Cursor cursor = getContacts();
+
+        while (cursor.moveToNext()) {
+
+            String displayName = cursor.getString(cursor
+                    .getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+            contactView.append("Name: ");
+            contactView.append(displayName);
+            contactView.append("\n");
+        }
+    }
+
+    private Cursor getContacts() {
+        // Run query
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        String[] projection = new String[] { ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME };
+        String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = '"
+                + ("1") + "'";
+        String[] selectionArgs = null;
+        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME
+                + " COLLATE LOCALIZED ASC";
+
+
+        return managedQuery(uri, projection, selection, selectionArgs,
+                sortOrder);
+    }
+
+}
